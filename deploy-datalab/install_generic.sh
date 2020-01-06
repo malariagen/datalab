173ec4300035a3b682d95bcfc457f7e57b33d007
root <root@instance-1.c.malariagen-jupyterhub.internal>
2020-01-06 11:16:42 +0000

    clean up deploy dir

--- deploy-datalab/install_generic.sh
+++
@@ -1,9 +0,0 @@
-# Suggested values: advanced users of Kubernetes and Helm should feel
-# free to use different values.
-RELEASE=jhub
-NAMESPACE=jhub
-
-helm upgrade --install $RELEASE jupyterhub/jupyterhub \
-  --namespace $NAMESPACE  \
-  --version 0.7.0 \
-  --values config.yaml
